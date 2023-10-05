a0995db51c72bab500fc111fef91ae49adb96f69
Jack Gaark <jack.gaarkeuken@gmail.com>
2023-10-04 23:57:58 -0400

    adding images/layout/routes

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let divStyle = ``;
+
+  let div2Style = ``;
+
+  let aStyle = ``;
+
+  let div3Style = ``;
+
+  onMount(() => {
+    if (window.innerWidth <= 991) {
+      divStyle += 'max-width: 100%;';
+      aStyle += 'font-size: 40px;';
+      div3Style += 'max-width: 100%;';
+    }
+  });
+
+</script>
+
+
+<section class="main-container">
+  <div class="image-wrapper">
+    <a href="..." class="title">Welcome</a>
+    <div class="description">
+      Lorem ipsum dolor sit amet consectetur. Leo justo enim et in. Aliquam at feugiat tortor purus quis eu ultrices quis tincidunt. Tellus integer egestas lectus ac.
+    </div>
+  </div>
+</section>
+
+
+<style>
+  .main-container {
+    justify-content: center;
+    align-items: center;
+    align-self: stretch;
+    display: flex;
+    margin-right: 1px;
+    padding-top: 40px;
+    padding-right: 20px;
+    padding-bottom: 40px;
+    padding-left: 20px;
+    flex-direction: column;
+    background-color: #bde3ff;
+  }
+
+  .image-wrapper {
+    align-items: flex-start;
+    align-self: center;
+    display: flex;
+    width: 736px;
+    max-width: 100%;
+    margin-top: 107px;
+    margin-bottom: 107px;
+    flex-direction: column;
+  }
+
+  .title {
+    color: #000;
+    font-family: Inter, sans-serif;
+    font-size: 48px;
+    font-weight: 500;
+    line-height: 150%;
+    letter-spacing: -0.528px;
+    align-self: start;
+    width: 209px;
+  }
+
+  .description {
+    flex: 1 0 0;
+    color: #000;
+    font-family: Inter, sans-serif;
+    font-size: 36px;
+    font-weight: 500;
+    line-height: 150%;
+    letter-spacing: -0.396px;
+    align-self: start;
+    margin-top: 18px;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script>
+    import "../styles.css"
+    import logo from "$lib/images/logo.svg"
+    import shoppingCart from "$lib/images/shoppingCart.svg"
+    import Hero from "../lib/components/Hero.svelte";
+
+</script>
+
+<div class="header">
+  <img src={logo} alt="ward logo"/>
+  <nav class="nav">
+    <ul class="nav__list">
+      <li class="nav__list-item"><a href="/">About</a></li>
+      <li class="nav__list-item"><a href="/">Philosophy</a></li>
+      <li class="nav__list-item"><a href="/">FAQ</a></li>
+      <li class="nav__list-item"><a href="/">Contact</a></li>
+    </ul>
+  </nav>
+  <a class="header__link" href="/">
+      <img src={shoppingCart} alt="Shopping cart">
+</div>
+
+<main>
+    <slot />
+</main>
+
+<style>
+  .header {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 0px 0px;
+    height: 96px;
+    position: relative;
+    background-color: #BDE3FF;
+}
+/* .header__link {
+  font-size: 40px;
+  font-weight: bold;
+} */
+.nav {
+  width: 1120px;
+  display: inline-block;
+  justify-self: flex-start;
+}
+.nav__list {
+  width: 1120px;
+  display: flex;
+  flex-wrap: wrap;
+  height: 36px;
+  align-items: center;
+  gap: 16px 32px;
+  position: relative;
+}
+
+.nav__list-item {
+  margin-inline-end: 36px;
+  font-size: 20px;
+}
+
+</style>
+
+  
